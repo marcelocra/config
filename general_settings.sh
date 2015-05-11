835a5df966833471aba71ea56fc6878af1baba72
Marcelo Almeida <marcelocralmeida@gmail.com>
2015-05-10 22:10:14 -0300

    Update bash

--- general_settings.sh
+++ general_settings.sh
@@ -4,24 +4,44 @@
 source ~/.dotfiles/ansi_colors.sh
 source ~/.dotfiles/prompt_style.sh
 
-# Aliases.
-
-## Vim.
-function run_gvim() {
-    if [ $# -eq 0 ];then
-        vim -g --remote-silent .
-    else
-        vim -g --remote-silent "$@"
-    fi
-}
+############################
+##### Aliases.
+############################
 
 alias vi="vim.tiny -u NONE"
 alias vim="vim"
-alias gvim="run_gvim"
+alias gvim="vim -g --remote-silent"
 
-## Git.
+# Git.
 alias gb="git branch"
 alias gco="git checkout"
 alias gd="git diff"
 alias gl="git log"
 alias gs="git status"
+
+# Better way to print files
+alias t="tree -L 1"
+alias tl="tree -L"
+
+# Allow proper coloring while using tmux
+#alias tmux="tmux"
+alias tmux="TERM=screen-256color-bce tmux"
+#alias tmux="TERM=screen-256color-bce; tmux -2"
+#alias tmux="tmux -2"
+
+############################
+##### Settings.
+############################
+
+# Make CapsLock a new Ctrl.
+setxkbmap -option ctrl:nocaps
+
+############################
+##### Environment.
+############################
+
+# Add Google AppEngine to PATH.
+export PATH="$HOME/Development/Tools/google_appengine:$PATH"
+
+# Add Leiningen.
+export PATH="$PATH:$HOME/Development/Tools/lein/"
