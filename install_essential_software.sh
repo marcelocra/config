af12155a2d72e17b5afb106ccb37cdf755eb3d41
Marcelo Almeida <marcelocralmeida@gmail.com>
2016-04-01 00:04:14 -0300

    Change sensitivity, add comments and remove forced yes

--- install_essential_software.sh
+++ install_essential_software.sh
@@ -1,23 +1,32 @@
 #!/usr/bin/env bash
 
 essential_softwares=(
+    # -----
+    # Dev.
+    # -----
+
     curl
     git
-    # Allows easy control of audio output devices. Install if necessary.
-    #pavucontrol
     # Makes it easier for programs that interact with the command line,
-    # such as Clojure's REPL.
+    # such as Clojure REPL.
     rlwrap
     tmux
+    vim-gtk
+
+    # ---------
+    # Utility.
+    # ---------
+
     # Shows a tree of all inner directories and files.
     tree
-    vim-gtk
     # Allows tmux to properly deal with the clipboard.
     xsel
+    # Allows easy control of audio output devices. Install if necessary.
+    #pavucontrol
 )
 
 for essential_software in ${essential_softwares[@]}; do
     echo "Installing $essential_software..."
-    sudo apt-get install -y $essential_software
+    sudo apt-get install $essential_software
     echo
 done
