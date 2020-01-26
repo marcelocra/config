99bb0e40617f8c55f8c3af7b27730e9c29878de3
Marcelo Almeida <marcelocralmeida@gmail.com>
2020-01-26 12:24:23 -0300

    Remove unused stuff when migrating to zsh

--- main.sh
+++ main.sh
@@ -6,7 +6,9 @@
 setup_expert_mouse() {
     local device_id
 
-    device_id="$(xinput list | sed -nre 's/.*Kensington\ Expert\ Mouse.*id\=([0-9]+).*/\1/p' | head -n1)"
+    device_id=$(xinput list \
+        | sed -nre 's/.*Kensington\ Expert\ Mouse.*id\=([0-9]+).*/\1/p' \
+        | head -n1)
     if [ ! -z "$device_id" ]; then
         xinput set-button-map $device_id 8 1 3 4 5 6 7 9 0 0
     fi
@@ -18,154 +20,53 @@ setup_expert_mouse
 setup_evoluent_mouse() {
     local device_id
 
-    device_id="$(xinput | sed -nre 's/.*Evoluent\ VerticalMouse.*id\=([0-9]+).*/\1/p' | head -n1)"
+    device_id=$(xinput \
+        | sed -nre 's/.*Evoluent\ VerticalMouse.*id\=([0-9]+).*/\1/p' \
+        | head -n1)
     if [ ! -z "$device_id" ]; then
         xinput set-button-map $device_id 1 2 3 4 5 6 7 8 10 9
     fi
 }
 setup_evoluent_mouse
 
-
-# Exports.
-export PATH="${PATH}:/snap/bin"
-
-#-------------------------------------------------------------------------------
-# Prompt colors.
-#-------------------------------------------------------------------------------
-
-RS="\[\033[0m\]"    # reset
-HC="\[\033[1m\]"    # hicolor
-UL="\[\033[4m\]"    # underline
-INV="\[\033[7m\]"   # inverse background and foreground
-FBLK="\[\033[30m\]" # foreground black
-FRED="\[\033[31m\]" # foreground red
-FGRN="\[\033[32m\]" # foreground green
-FYEL="\[\033[33m\]" # foreground yellow
-FBLE="\[\033[34m\]" # foreground blue
-FMAG="\[\033[35m\]" # foreground magenta
-FCYN="\[\033[36m\]" # foreground cyan
-FWHT="\[\033[37m\]" # foreground white
-BBLK="\[\033[40m\]" # background black
-BRED="\[\033[41m\]" # background red
-BGRN="\[\033[42m\]" # background green
-BYEL="\[\033[43m\]" # background yellow
-BBLE="\[\033[44m\]" # background blue
-BMAG="\[\033[45m\]" # background magenta
-BCYN="\[\033[46m\]" # background cyan
-BWHT="\[\033[47m\]" # background white
-
-#-------------------------------------------------------------------------------
-# Functions.
-#-------------------------------------------------------------------------------
-
-function get_git_branch() {
-    local branch
-
-    if branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null); then
-        if [[ "$branch" == "HEAD" ]]; then
-            branch='detached*'
-        fi
-    else
-        # Not in a git repository. Show nothing.
-        echo ''
+reduce_speed_of_logitech_mouse() {
+    local device_id
+    device_id=$(xinput \
+        | sed -nre 's/.*Logitech.*Mouse.*id\=([0-9]+).*/\1/p' \
+        | head -n1)
+    if [ -z "$device_id" ]; then
         return
     fi
-    echo "$branch"
-}
 
-function set_bash_prompt() {
-    # Grab the return code of the last ran command.
-    local ret_code="$?"
-    local git_branch="$(get_git_branch)"
-
-    if [ "${git_branch}" != "" ]; then
-        git_branch="($git_branch)"
-    else
-        git_branch=""
+    local prop_id
+    prop_id=$(xinput list-props $device_id \
+        | sed -nre 's/.*Accel\ Speed\ \(([0-9]+)\).*/\1/p' \
+        | head -n1)
+    if [ -z "$prop_id" ]; then
+        return
     fi
 
-    if [ $ret_code -eq 0 ]; then
-        PS1="$FGRN"
-    else
-        PS1="$FRED"
-    fi
-
-    # 21 is the size of the date and time shown in the separator line.
-    for i in $(seq 1 $((COLUMNS-21))); do
-        PS1="$PS1-"
-    done
-    PS1="$PS1[\D{%F %T}]"
-    # Reset prompt color.
-    PS1="$PS1$RS"
-
-    local prompt_char
-    local user_host
-    local working_dir
-
-    prompt_char="$RS$HC$FCYN$"
-    user_host="$USER@$HOSTNAME"
-    working_dir="[\w]"
-
-    # Prompt first line.
-    PS1="$PS1$user_host $working_dir $git_branch\n"
-    # Prompt second line.
-    PS1="$PS1$prompt_char$RS "
+    xinput set-prop $device_id $prop_id -0.75  # I found this value to be good.
 }
+reduce_speed_of_logitech_mouse
 
-function simple_prompt () {
-    local return_code="$?"
-    local time_len=8  # 00:00:00
-    local date_len=10  # 2000/01/01
-    local num_spaces=$(( COLUMNS - time_len - date_len - ${#USER} - \
-        ${#HOSTNAME} - 1 ))  # `1` for the `@` sign at user@host.
-    local initial_spaces=$(( (num_spaces / 2) - 1 ))
-    local remaining_spaces=$(( num_spaces - initial_spaces ))
+# Exports.
+# --------
 
-    PS1="$BWHT"
+export PATH="${PATH}:/snap/bin"
+export PATH="${PATH}:${HOME}/bin"
 
-    if [ $return_code -ne 0 ]; then
-        PS1="$BRED"
-    fi
-
-    PS1="$FBLK$PS1\D{%F}"
-    for i in $(seq $initial_spaces); do
-        PS1="$PS1 "
-    done
-    PS1="$PS1\u@$HOSTNAME"
-    for i in $(seq $remaining_spaces); do
-        PS1="$PS1 "
-    done
-    PS1="$PS1\D{%T}$RS\n[\w]\n$ "
-}
-
-function docker_clean_all () {
-    docker stop $(docker ps -aq) && docker rm $(docker ps -aq)
-}
-
-#-------------------------------------------------------------------------------
 # Aliases.
-#-------------------------------------------------------------------------------
+# --------
 
-# Tmux stuff.
-# Previous version that used to work.
-#alias tmux="TERM=screen-256color-bce tmux"
-# New version, that is currently working.
 alias tmux="TERM=xterm-256color tmux"
 
-# File stuff.
 alias ll="ls -lFa"
 alias l="ls -l"
-alias trash="gvfs-trash"
 
-# Vim stuff.
 alias vi="vim.tiny -u NONE"
 
 alias inst="sudo apt install"
 alias purge="sudo apt purge"
 
-#-------------------------------------------------------------------------------
-# Commands to improve setup.
-#-------------------------------------------------------------------------------
-
-# Run the given function before generating the prompt.
-PROMPT_COMMAND="simple_prompt"
+alias eg="egrep"
