9cd8bb241d3be2bbacd7af4756b5e73ece798b95
Marcelo Almeida <marcelocralmeida@gmail.com>
2020-02-01 01:53:27 -0300

    Change code font, update scripts and readme
    changes:
    - (consolas -> source code pro) in the browser
    - include more essential steps in readme
    - include more essential installs in install_essentials
    - update readme for new .local/bin and for aliases

--- install_essentials.sh
+++ install_essentials.sh
@@ -1,25 +1,67 @@
 #!/usr/bin/env bash
 
+essential_stuff() {
+    local tmp=$(mktemp)
+
+    local stuff
+    stuff=(
+        # programs
+        build-essential
+        curl
+        git
+        tmux
+        tree
+        xsel
+        python3-dev
+        python3-pip
+        # fonts
+        fonts-firacode
+        fonts-monoid
+        fonts-hack
+        fonts-source-code-pro
+        fonts-liberation
+        fonts-inconsolata
+        fonts-croscore
+        fonts-crosextra-caladea
+        fonts-crosextra-carlito
+    )
+
+    echo 'Installing programs and fonts...' | tee -a $tmp
+
+    for i in ${stuff[@]}; do
+        echo "Installing $i..." | tee -a $tmp
+        sudo apt install -y $i >> $tmp 2>&1
+        if [ $? -ne 0 ]; then
+            echo "Failed to install $i" | tee -a $tmp
+        fi
+    done
+
+    echo 'Installing Python packages...' | tee -a $tmp
+    local python_pkgs
+    python_pkgs=(
+        setuptools
+        wheel
+        ipython  # should be last
+    )
+
+    for i in ${python_pkgs[@]}; do
+        echo "Installing $i..." | tee -a $tmp
+        pip3 install $i >> $tmp 2>&1
+        if [ $? -ne 0 ]; then
+            echo "Failed to install $i" | tee -a $tmp
+        fi
+    done
+
+    while true; do
+        read -p "Want to see the logs? (y/n) " yn 
+        case $yn in
+            [Yy]* ) cat $tmp; exit;;
+            [Nn]* ) exit;;
+            * ) echo 'Please answer yes or no.';;
+        esac
+    done
+}
+
 sudo apt update \
-    && sudo apt install -y \
-        build-essential \
-        curl \
-        git \
-        tmux \
-        tree \
-        xsel \
-    && sudo apt install -y \
-        fonts-firacode \
-        fonts-monoid \
-        fonts-source-code-pro \
-        fonts-hack \
-        fonts-liberation \
-        fonts-inconsolata \
-        fonts-croscore \
-        fonts-crosextra-caladea \
-        fonts-crosextra-carlito \
-    && sudo apt install -y \
-        python3-dev \
-        python3-pip \
-        pip install setuptools wheel ipython
+    && essential_stuff
 
