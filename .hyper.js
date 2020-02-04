02dd14a19bdb5344d7e104f4f0897569b0b509fb
Marcelo Almeida <marcelocralmeida@gmail.com>
2020-02-04 00:19:06 -0300

    Change hyper background to black

--- .hyper.js
+++ .hyper.js
@@ -43,7 +43,7 @@ module.exports = {
 
     // terminal background color
     // opacity is only supported on macOS
-    backgroundColor: '#333',
+    backgroundColor: '#000',
 
     // terminal selection color
     selectionColor: 'rgba(248,28,229,0.3)',
