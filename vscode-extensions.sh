ae019ce09d57123689135839a0ddfeeb88e75935
Marcelo Almeida <marcelocralmeida@gmail.com>
2020-02-13 10:44:45 -0300

    Remove dup from extension list

--- vscode-extensions.sh
+++ vscode-extensions.sh
@@ -1,10 +1,12 @@
-#!/usr/bin/env -S bash -e
+#!/usr/bin/env bash
 # Run the following command to save the list of extensions from one machine.
 #   Unix:
 #       code --list-extensions | xargs -L 1 echo code --install-extension
 #   Windows:
 #       code --list-extensions | % { "code --install-extension $_" }
 # Then put it here.
+
+set -e
 
 code --install-extension auchenberg.vscode-browser-preview
 code --install-extension CoenraadS.bracket-pair-colorizer-2
@@ -13,7 +15,8 @@ code --install-extension eamodio.gitlens
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension msjsdiag.debugger-for-chrome
 code --install-extension ms-python.python
-code --install-extension ms-python.python 
 code --install-extension ms-vscode.Go
 code --install-extension ms-vscode.sublime-keybindings
 code --install-extension vscodevim.vim
+
+set +e
