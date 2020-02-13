f542f0a7bdf75b95cb937e130e88c325541ee3cd
Marcelo Almeida <marcelocralmeida@gmail.com>
2020-02-13 07:16:08 -0300

    Update extensions and remove light colorscheme

--- vscode-extensions.sh
+++ vscode-extensions.sh
@@ -6,12 +6,14 @@
 #       code --list-extensions | % { "code --install-extension $_" }
 # Then put it here.
 
+code --install-extension auchenberg.vscode-browser-preview
+code --install-extension CoenraadS.bracket-pair-colorizer-2
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension eamodio.gitlens
 code --install-extension ms-azuretools.vscode-docker
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension ms-python.python
 code --install-extension ms-python.python 
 code --install-extension ms-vscode.Go
 code --install-extension ms-vscode.sublime-keybindings
-code --install-extension msjsdiag.debugger-for-chrome
 code --install-extension vscodevim.vim
-
