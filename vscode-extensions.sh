e8b40bab0f305c3e596efa1c72bb7fbc8d6a347c
Marcelo Almeida <marcelocralmeida@gmail.com>
2020-02-13 07:02:56 -0300

    Save vscode extensions in dotfiles

---
+++ vscode-extensions.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env -S bash -e
+# Run the following command to save the list of extensions from one machine.
+#   Unix:
+#       code --list-extensions | xargs -L 1 echo code --install-extension
+#   Windows:
+#       code --list-extensions | % { "code --install-extension $_" }
+# Then put it here.
+
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension eamodio.gitlens
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-python.python 
+code --install-extension ms-vscode.Go
+code --install-extension ms-vscode.sublime-keybindings
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension vscodevim.vim
+
