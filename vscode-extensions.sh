7212462ccbe8d2529886c9f34a0cd57d044ae1e6
Marcelo Almeida <marcelocralmeida@gmail.com>
2020-06-11 22:18:26 -0300

    Update installed extensions

--- vscode-extensions.sh
+++ vscode-extensions.sh
@@ -11,29 +11,33 @@ set -e
 
 EXTENSIONS_FILE="vscode-extensions.txt"
 
-if [[ $1 == *save* ]]; then
+if [[ $1 == save ]]; then
   EXTENSIONS_LIST=$(mktemp)
   code --list-extensions | xargs -L 1 echo > $EXTENSIONS_LIST
   code-insiders --list-extensions | xargs -L 1 echo >> $EXTENSIONS_LIST # Note that this is an append.
   cat $EXTENSIONS_LIST | sort | uniq | tee $EXTENSIONS_FILE
 fi
 
-if [[ $1 == *inst* ]]; then
+if [[ $1 == inst* ]]; then
   if [ ! -f $EXTENSIONS_FILE ]; then
     echo "You don't have an extensions file"
     exit 1
   fi
 
-  echo '--------------------------------'
-  echo '| Installing Code extensions...|'
-  echo '--------------------------------'
-  cat $EXTENSIONS_FILE | xargs -L 1 code --install-extension
-  echo '... done!'
-  echo '-----------------------------------------'
-  echo '| Installing Code Insiders extensions...|'
-  echo '-----------------------------------------'
-  cat $EXTENSIONS_FILE | xargs -L 1 code-insiders --install-extension
-  echo '... done!'
+  if [[ -z $2 || $2 == code ]]; then
+    echo '--------------------------------'
+    echo '| Installing Code extensions...|'
+    echo '--------------------------------'
+    cat $EXTENSIONS_FILE | xargs -L 1 code --install-extension
+    echo '... done!'
+  fi
+  if [[ -z $2 || $2 == codi ]]; then
+    echo '-----------------------------------------'
+    echo '| Installing Code Insiders extensions...|'
+    echo '-----------------------------------------'
+    cat $EXTENSIONS_FILE | xargs -L 1 code-insiders --install-extension
+    echo '... done!'
+  fi
 fi
 
 set +e
